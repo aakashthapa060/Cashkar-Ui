f6810a1a479d876e5cefb018dc47626eb2e158b6
Aakash Thapa <aakashmanm3@gmail.com>
2021-07-20 19:51:30 +0545

    Dashboard Added
    Dashboard added but not complete

---
+++ src/js/dashboard.js
@@ -0,0 +1,37 @@
+let sideNavOpen = false;
+function sideNavOpenClose(){
+    let sideNav = $(".dash_board .dashboard_nav");
+    let hamBurger = $(".dash_board .top_nav .wrapper .ham_burger_menu")
+    hamBurger.click(() => {
+        if(sideNavOpen === true){
+            sideNav.show()
+            sideNavOpen = false
+        }
+        else if(sideNavOpen === false){
+            sideNav.hide()
+            sideNavOpen = true
+        }
+    })
+
+}
+
+let accountSetting_open = false
+
+function TopNavExtraMenu(){
+    let topNav_optionMenu = $(".top_nav .wrapper .user_content .user_info")
+    topNav_optionMenu.click(() => {
+        if(accountSetting_open === false){
+            $(".top_nav .wrapper .user_content .profile_drop_down").fadeIn()
+            accountSetting_open = true
+        }
+        else if(accountSetting_open === true){
+            $(".top_nav .wrapper .user_content .profile_drop_down").fadeOut()
+            accountSetting_open = false
+
+        }
+    })
+}
+window.addEventListener("load", () => {
+    sideNavOpenClose()
+    TopNavExtraMenu()
+})
